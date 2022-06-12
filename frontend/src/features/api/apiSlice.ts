a1c1d1093329d0c5416c824d4848033f6d78febd
natserract <benjaminstwo@gmail.com>
2022-06-12 13:06:41 +0800

    feat: setup deployment on local server

--- frontend/src/features/api/apiSlice.ts
+++ frontend/src/features/api/apiSlice.ts
@@ -2,7 +2,9 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react";
 
 export const emptySplitApi = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "/api/v1" }),
+    baseQuery: fetchBaseQuery({ baseUrl:
+      `${process.env.BASE_URL || "http://amanahtoko.local:5000/api/v1"}`
+    }),
     tagTypes: ["Category", "Product", "Purchase", "Sale", "Supplier", "Transfer"],
     endpoints: () => ({}),
 })
