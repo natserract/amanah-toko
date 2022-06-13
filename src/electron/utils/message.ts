90daf035667842aaafa782c4799b5ecaecf83715
natserract <benjaminstwo@gmail.com>
2022-06-13 09:43:38 +0800

    feat: add lists of ipc actions messages

---
+++ src/electron/utils/message.ts
@@ -0,0 +1,44 @@
+// ipcRenderer.send(...)
+export enum IPC_MESSAGES {
+  OPEN_MENU = 'open-menu',
+  OPEN_SETTINGS = 'open-settings',
+  OPEN_EXTERNAL = 'open-external',
+  SHOW_ITEM_IN_FOLDER = 'show-item-in-folder',
+  RELOAD_MAIN_WINDOW = 'reload-main-window',
+}
+
+// ipcRenderer.invoke(...)
+export enum IPC_ACTIONS {
+  GET_OPEN_FILEPATH = 'open-dialog',
+  GET_SAVE_FILEPATH = 'save-dialog',
+  GET_DIALOG_RESPONSE = 'show-message-box',
+  GET_ENV = 'get-env',
+  SAVE_HTML_AS_PDF = 'save-html-as-pdf',
+  SAVE_DATA = 'save-data',
+  SHOW_ERROR = 'show-error',
+  SEND_ERROR = 'send-error',
+  GET_LANGUAGE_MAP = 'get-language-map',
+  CHECK_FOR_UPDATES = 'check-for-updates',
+  GET_FILE = 'get-file',
+  GET_CREDS = 'get-creds',
+  GET_DB_LIST = 'get-db-list',
+  DELETE_FILE = 'delete-file',
+  // Database messages
+  DB_CREATE = 'db-create',
+  DB_CONNECT = 'db-connect',
+  DB_CALL = 'db-call',
+  DB_BESPOKE = 'db-bespoke',
+  DB_SCHEMA = 'db-schema',
+}
+
+// ipcMain.send(...)
+export enum IPC_CHANNELS {
+  MAIN_PROCESS_ERROR = 'main-process-error',
+  CONSOLE_LOG = 'console-log',
+}
+
+export enum DB_CONN_FAILURE {
+  INVALID_FILE = 'invalid-file',
+  CANT_OPEN = 'cant-open',
+  CANT_CONNECT = 'cant-connect',
+}
