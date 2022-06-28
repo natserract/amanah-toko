10730ac3605b9c82a86ab9677df22b9d027f19c1
natserract <benjaminstwo@gmail.com>
2022-06-28 08:33:34 +0800

    feat: setup deploying frontend

--- frontend/src/features/api/apiSlice.ts
+++ frontend/src/features/api/apiSlice.ts
@@ -1,9 +1,14 @@
 import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/dist/query/react';
 
+const env: "development" | "test" | "production" = process.env.NODE_ENV;
+
+const devApiUrl = process.env.BASE_URL || 'http://amanahtoko.local:5000/api/v1';
+const prodApiUrl = process.env.BASE_PROD_URL;
+
 export const emptySplitApi = createApi({
   reducerPath: 'api',
   baseQuery: retry(fetchBaseQuery({
-    baseUrl: `${process.env.BASE_URL || 'http://amanahtoko.local:5000/api/v1'}`,
+    baseUrl: `${env === "development" ? devApiUrl : prodApiUrl}`,
   }), {
     maxRetries: 3
   }),
