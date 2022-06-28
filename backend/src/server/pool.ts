bdc12c78eede21bbedb36bc4cc483d7733077225
natserract <benjaminstwo@gmail.com>
2022-06-28 08:16:28 +0800

    feat: setup deploying backend

--- backend/src/server/pool.ts
+++ backend/src/server/pool.ts
@@ -31,14 +31,14 @@ const appHost = createVirtualHost(
 export const initPool = async () => {
   const app = express();
   const port = normalizePort(process.env.PORT || '5000');
-  const host = process.env.host || "pure-spire-61458.herokuapp.com"
+  const host = process.env.host || "localhost"
   app.set('port', port);
   app.set('host', host);
 
   const server = http.createServer(app);
   server.listen(port, () => {
     console.log(
-      `Server running at http://amanahtoko.local:%d: in %s mode`,
+      `Server running at http://${host}:%d: in %s mode`,
       port,
       host,
       app.settings.env,
