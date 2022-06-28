76eb594b2a083e6539470987c17feafc6be8280e
natserract <benjaminstwo@gmail.com>
2022-06-28 08:47:59 +0800

    feat: setup deploying frontend ~ environment

--- frontend/src/features/api/apiSlice.ts
+++ frontend/src/features/api/apiSlice.ts
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/dist/query/react';
 
-const env: "development" | "test" | "production" = process.env.NODE_ENV;
+const env: "development" | "test" | "production" | any = process.env.REACT_NODE_ENV;
 
 const devApiUrl = process.env.BASE_URL || 'http://amanahtoko.local:5000/api/v1';
-const prodApiUrl = process.env.BASE_PROD_URL;
+const prodApiUrl = process.env.BASE_PROD_URL || "https://pure-spire-61458.herokuapp.com/api/v1";
 
 export const emptySplitApi = createApi({
   reducerPath: 'api',
